0878d546c544e960dab6610b2a51ea54842e6ccc
Nick Langridge <nicklangridge@hotmail.com>
2019-01-22 19:07:31 +0000

    unicode fixups

---
+++ server/utils/db/fixup_album_name.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+use feature 'say';
+use lib 'lib';
+use utf8::all;
+
+use Data::Dumper;
+use FeedMe::Metadata::Spotify;
+use FeedMe::MySQL qw(dbh);
+use FeedMe::Utils::Slug qw(slug);
+
+my $spotify = FeedMe::Metadata::Spotify->new;
+
+my @rows = dbh->query('SELECT * FROM album WHERE name != CONVERT(BINARY CONVERT(name USING latin1) USING utf8)')->hashes;
+
+say 'found ' . scalar(@rows) . ' albums with special chars';
+
+foreach my $row (@rows) {
+  say ;
+  my $album = $spotify->_fetch('album', $spotify->_get_id('album', $row->{uri}));
+  say "$row->{album_id} $row->{name} --> $album->{name}";
+  dbh->query('UPDATE album SET name = ?, slug = ? WHERE album_id = ?', $album->{name}, slug($album->{name}), $row->{album_id});
+}
+
+say "done";
+
+exit 0;
+
---
+++ server/utils/db/fixup_artist_name.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+use feature 'say';
+use lib 'lib';
+use utf8::all;
+
+use Data::Dumper;
+use FeedMe::Metadata::Spotify;
+use FeedMe::MySQL qw(dbh);
+use FeedMe::Utils::Slug qw(slug);
+
+my $spotify = FeedMe::Metadata::Spotify->new;
+
+my @rows = dbh->query('SELECT * FROM artist WHERE name != CONVERT(BINARY CONVERT(name USING latin1) USING utf8)')->hashes;
+
+say 'found ' . scalar(@rows) . ' artits with broken utf-8 encoding';
+
+foreach my $row (@rows) {
+  say ;
+  my $artist = $spotify->_fetch('artist', $spotify->_get_id('artist', $row->{uri}));
+  say "$row->{artist_id} $row->{name} --> $artist->{name}";
+  dbh->query('UPDATE artist SET name = ?, slug = ? WHERE artist_id = ?', $artist->{name}, slug($artist->{name}), $row->{artist_id});
+}
+
+say "done";
+
+exit 0;
+
