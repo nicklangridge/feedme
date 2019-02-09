824ffd6b78df0ab34ab412ea2a2d8279ff14868b
Nick Langridge <nicklangridge@hotmail.com>
2019-02-09 08:08:06 +0000

    Add delay to spinner

--- client/scripts/components/Spinner.js
+++ client/scripts/components/Spinner.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 
 class Spinner extends Component {
+  constructor(props) {
+    super(props);
+    this.setVisible = this.setVisible.bind(this);
+
+    this.state = {
+      visible: false,
+    };
+
+    this.timer = setTimeout(this.setVisible, 250);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  setVisible() {
+    this.setState({visible: true});
+  }
+
+  
   render() {
+    const {visible} = this.state;
+    
+    if (!visible) {
+      return null;
+    }
+    
     return (
       <div className="spinner-container">
         <div className="spinner"></div>
