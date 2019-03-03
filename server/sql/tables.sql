de689ba66221a30957387bc3a427e14e0d89081a
Nick Langridge <nicklangridge@hotmail.com>
2019-03-03 07:53:42 +0000

    Remove duplicate table def

--- server/sql/tables.sql
+++ server/sql/tables.sql
@@ -103,19 +103,3 @@ CREATE TABLE `feed` (
   PRIMARY KEY (`feed_id`),
   CONSTRAINT unique_slug UNIQUE (`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for `genre_index`
--- ----------------------------
-DROP TABLE IF EXISTS `feed`;
-CREATE TABLE `feed` (
-  `feed_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
-  `homepage_url` varchar(255) NOT NULL,
-  `module` varchar(40) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `public` tinyint(4) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`feed_id`),
-  CONSTRAINT unique_slug UNIQUE (`slug`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
