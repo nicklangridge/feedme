85fd96bf4585cd1570ed92d3c31d9460332aa638
Nick Langridge <nicklangridge@hotmail.com>
2019-03-11 13:19:48 +0000

    Retire some feeds

--- server/lib/FeedMe/Feed/Spotinews.pm
+++
@@ -1,9 +0,0 @@
-package FeedMe::Feed::Spotinews;
-use Moo;
-with 'FeedMe::Role::Feed::XML';
-
-sub name         { 'Spotinews' };
-sub url          { 'https://spotinews.wordpress.com/feed' };
-sub homepage_url { 'https://spotinews.wordpress.com' };
-
-1;
--- server/lib/FeedMe/Feed/TapeFear.pm
+++
@@ -1,9 +0,0 @@
-package FeedMe::Feed::TapeFear;
-use Moo;
-with 'FeedMe::Role::Feed::XML';
-
-sub name         { 'TapeFear' };
-sub url          { 'http://feeds.feedburner.com/TapeFear' };
-sub homepage_url { 'http://www.tapefear.com' };
-
-1;
