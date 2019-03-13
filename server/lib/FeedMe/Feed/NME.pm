d64c6a1e8e9fb27f6d4072a1e76a1c8c89ce1065
Nick Langridge <nicklangridge@hotmail.com>
2019-03-13 13:03:00 +0000

    Fix NME feed

--- server/lib/FeedMe/Feed/NME.pm
+++ server/lib/FeedMe/Feed/NME.pm
@@ -1,21 +1,39 @@
 package FeedMe::Feed::NME;
 use Moo;
 use Method::Signatures;
+use Mojo::DOM;
 use Text::Trim;
 
-with 'FeedMe::Role::Feed::XML';
+with 'FeedMe::Role::Feed::DOM';
 
 sub name         { 'NME' };
-sub url          { 'http://www.nme.com/reviews/feed/' };
+sub url          { 'https://www.nme.com/reviews?review_category=album' };
 sub homepage_url { 'http://www.nme.com' };
 
-method extract_artist_and_album ($title) {
-  $title =~ s/\s+/ /g;
-  $title =~ /(.+?)\s+(?:\x{2013}|\x{2014}|-|–)\s+\x{2018}(.+)\x{2019}/;
+method parallel_parsers { 3 }
+
+method extract_entry_urls ($dom) {
+  my @urls = @{ $dom->find('*')->map(attr => 'href')->compact };
+  my %urls = map { $_ => 1 } grep {$_ =~ /.*www\.nme\.com\/reviews\/album\/.+/} @urls;
+
+  return keys %urls;
+}
+
+method parse_entry ($url) {
+  # pick out the artist and album name from the html of the article
+  my $html  = $self->_get($url)      || die "Failed to fetch page [$url]: $!\n";
+  my $dom   = Mojo::DOM->new($html)  || die "Failed to parse html: $!\n";  
+  my $title = eval { $dom->at('.title-primary')->all_text } || '';
+  
+  # artist - ‘album’ review
+  $title =~ s/review\s*$//;
+  $title =~ s/[\x{2018}\x{2019}‘’']//g;
+  #warn $title;
+  
   return {
-    artist => trim $1, 
-    album  => trim $2
-  };
+    title   => trim $title,
+    url     => trim $url,
+  }
 }
 
 1;
