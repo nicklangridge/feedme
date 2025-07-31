f6bf001ae26baec0cb10fe58c3ed9d62c8cb2ffe
Nick Langridge <nicklangridge@hotmail.com>
2025-07-31 17:10:24 +0000

    Add Igloo feed

---
+++ server/lib/FeedMe/Feed/Igloo.pm
@@ -0,0 +1,31 @@
+package FeedMe::Feed::Igloo;
+use Moo;
+use Method::Signatures;
+use Text::Trim;
+use FeedMe::Utils::Snippet qw(snippet);
+
+with 'FeedMe::Role::Feed::XML';
+
+sub name         { 'Igloo' };
+sub url          { 'https://igloomag.com/category/reviews/feed' };
+sub homepage_url { 'https://igloomag.com/' };
+
+method extract_artist_and_album ($title) {
+  $title =~ s/\s+/ /g;
+  $title =~ /(.+?)\s*\:\:\s+(.+?)\s+(EP\s+)?(\(.+)?/i;  
+  return {
+    artist => trim $1, 
+    album  => trim $2
+  };
+}
+
+method parse_entry ($entry) {
+  return {
+    title       => trim $entry->title,
+    url         => trim $entry->link,
+    description => snippet($entry->content->body, 200),
+  }
+}
+
+
+1;
